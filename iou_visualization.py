b12b337291c2c0060b1c0a9e025cc37ab938fbf2
beerys <sara.beery@gmail.com>
2017-05-02 09:50:17 -0700

    edited

--- iou_visualization.py
+++ iou_visualization.py
@@ -53,9 +53,9 @@ resized_pic = np.reshape(resized_pic, (1,img_rows,img_cols,3))
 y_pred = model.predict([resized_pic], batch_size=1, verbose=1)
 conf = y_pred[:,:,-1]
 conf = np.reshape(conf, (1419, 1))
-print(np.linalg.norm(conf))
-if(np.amax(conf, axis=0) is not 0):
-    conf = conf / np.amax(conf, axis=0)
+print(np.amax(conf))
+if(np.amax(conf) is not 0):
+    conf = conf / np.amax(conf)
 
 y_pred = y_pred[:,:,:-1]
 print(y_pred.shape)
@@ -71,7 +71,8 @@ for i in range(1419):
     boxes = [boxes[0]*h/img_rows, boxes[1]*w/img_cols, boxes[2]*h/img_rows, boxes[3]*w/img_cols]
     a = conf[i,:]
     print(a)
-    ax.add_patch(patches.Rectangle((boxes[3], boxes[0]), boxes[3] - boxes[1], boxes[2] - boxes[0], alpha=float(a), facecolor='green'))
+    if( a is not 0):
+        ax.add_patch(patches.Rectangle((boxes[3], boxes[0]), boxes[3] - boxes[1], boxes[2] - boxes[0], alpha=float(a), facecolor='green'))
 #plt.title('Bounding boxes in image ' + str(imNum))
 plt.axis('off')
 img_name = choice[0].split('/')[1]
