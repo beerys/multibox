fc6c806c4ff403497ea81e7d5f5fe8b80d42cecd
beerys <sara.beery@gmail.com>
2017-05-01 18:31:04 -0700

    edited

--- get_data_info.py
+++ get_data_info.py
@@ -13,11 +13,13 @@ def get_data_info(num_ims):
     class_file = source_dir + 'image_class_labels.txt'
     image_file = source_dir + 'images.txt'
     split_file = source_dir + 'train_test_split.txt'
+    bboxes = [line.rstrip().split() for line in open(source_dir + 'bounding_boxes.txt')]
     x_train = []
     y_train = []
     x_test = []
     y_test = []
     classes = []
+    bbox = {}
     f1 = open(class_file, 'r')
     f2 = open(image_file, 'r')
     f3 = open(split_file, 'r')
@@ -27,6 +29,8 @@ def get_data_info(num_ims):
         file_name = f2.readline().split()[1]
         class_name = file_name.split('/')[0]
         class_num = float(f1.readline().split()[1])-1
+        # print(bboxes[count])
+        bbox[file_name] = [float(bboxes[count][1]), float(bboxes[count][2]), float(bboxes[count][3]), float(bboxes[count][4])]
         if class_name not in classes:
             classes.append(class_name)
         if float(line[1]):
@@ -39,4 +43,4 @@ def get_data_info(num_ims):
     y_train = np.asarray(y_train)
     y_test = np.asarray(y_test)
 
-    return x_train, x_test, y_train, y_test, classes
+    return x_train, x_test, y_train, y_test, classes, bbox
--- hw4p1.py
+++ hw4p1.py
@@ -10,21 +10,19 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from get_data_info import get_data_info
-import loss
+from multibox_loss import custom_loss
+import skimage.io as imio
+import skimage.transform as tform
 
 import keras
 import tensorflow as tf
 from keras.models import Model
-from keras.layers import Dense, Input, GlobalAveragePooling2D, Conv2D
+from keras.layers import Dense, Input, GlobalAveragePooling2D, Conv2D, Flatten, Reshape
+from keras.layers.merge import concatenate
 from keras import backend as K
 #from keras.models import load_model
-#from sklearn.metrics import confusion_matrix
-from keras.callbacks import ModelCheckpoint, CSVLogger
-#from keras.callbacks import TensorBoard
 from keras.applications.inception_v3 import InceptionV3
-from keras.applications.resnet50 import ResNet50
-from keras.preprocessing.image import ImageDataGenerator
-import pickle
+
 
 
 batch_size = 32
@@ -35,6 +33,7 @@ img_rows = 299
 img_cols = 299
 size = (img_rows,img_cols)
 epochs = 10
+batches = 300
 
 if train_all_classes:
     num_ims = 11788
@@ -43,12 +42,15 @@ else:
     num_ims = 1115
     num_classes = 20
 
-image_folder = 'CUB_200_2011/CUB_200_2011/images'
+source_dir = 'CUB_200_2011/CUB_200_2011/'
+image_folder = source_dir + 'images'
 test_folder = 'Test'
 train_folder = 'Train'
-filepath = "Bird_Model_1.h5"
+model_name = 'Multibox_Bird_Model_1'
+filepath = model_name + '.h5'
 
-x_train_names, x_test_names, y_train, y_test, classes = get_data_info(num_ims)
+x_train_names, x_test_names, y_train, y_test, classes, bbox = get_data_info(num_ims)
+
 
 # convert class vectors to binary class matrices, subtract 1 to get correct classes
 y_train = keras.utils.to_categorical(y_train, num_classes)
@@ -64,83 +66,83 @@ x = base_model.output
 a = Conv2D(96, (1,1), activation='relu')(x)
 a = Conv2D(96, (3,3), padding='SAME', activation='relu')(a)
 loc_a = Conv2D(44, (1,1))(a)
+loc_a = Reshape(target_shape=(704,4))(loc_a)
 conf_a = Conv2D(11, (1,1))(a)
+conf_a = Reshape(target_shape=(704,1))(conf_a)
+out_a = concatenate([loc_a, conf_a])
 
 # #center path
 b = Conv2D(96, (3,3), padding='SAME', activation='relu')(x)
 b = Conv2D(96, (3,3), activation='relu')(b)
 loc_b = Conv2D(44, (1,1))(b)
+loc_b = Reshape(target_shape=(396,4))(loc_b)
 conf_b = Conv2D(11, (1,1))(b)
+conf_b = Reshape(target_shape=(396,1))(conf_b)
+out_b = concatenate([loc_b, conf_b])
 #
 # #right path
 c = Conv2D(256,(3,3), padding='SAME', strides=2, activation='relu')(x)
 
 c1 = Conv2D(128,(3,3), padding = 'SAME',activation='relu')(c)
 loc_c1 = Conv2D(44, (1,1))(c1)
+loc_c1 = Reshape(target_shape=(176,4))(loc_c1)
 conf_c1 = Conv2D(11, (1,1))(c1)
+conf_c1 = Reshape(target_shape=(176,1))(conf_c1)
+out_c1 = concatenate([loc_c1, conf_c1])
 #
 c2 = Conv2D(128, (1,1), activation='relu')(c)
 c2 = Conv2D(96, (2,2), activation='relu')(c2)
 loc_c2 = Conv2D(44, (1,1))(c2)
+loc_c2 = Reshape(target_shape=(99,4))(loc_c2)
 conf_c2 = Conv2D(11, (1,1))(c2)
+conf_c2 = Reshape(target_shape=(99,1))(conf_c2)
+out_c2 = concatenate([loc_c2, conf_c2])
 #
 c3 = Conv2D(128, (1,1), activation='relu')(c)
 c3 = Conv2D(96, (3,3), activation='relu')(c3)
 loc_c3 = Conv2D(44, (1,1))(c3)
+loc_c3 = Reshape(target_shape=(44,4))(loc_c3)
 conf_c3 = Conv2D(11, (1,1))(c3)
+conf_c3 = Reshape(target_shape=(44,1))(conf_c3)
+out_c3 = concatenate([loc_c3, conf_c3])
 #
-predictions = (loc_a, conf_a, loc_b, conf_b, loc_c1, conf_c1, loc_c2, conf_c2, loc_c3, conf_c3)
+out = concatenate([out_a, out_b, out_c1, out_c2, out_c3], axis=1)
 
+# predictions = (loc_a, conf_a, loc_b, conf_b, loc_c1, conf_c1, loc_c2, conf_c2, loc_c3, conf_c3)
+predictions = out
 # this is the model we will train
 model = Model(input=base_model.input, output=predictions)
-model.summary()
+# model.summary()
 # first: train only the top layers (which were randomly initialized)
 # i.e. freeze all convolutional InceptionV3 layers
 for layer in base_model.layers:
     layer.trainable = False
 
-#create checkpoints (after model has been compiled)
-# filepath = "weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
-# checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-# callbacks_list = [checkpoint]
-
-#create Tensorboard Logs
-#remember to pass this to your model while fitting!! model.fit(...inputs and parameters..., callbacks=[tbCallBack])
-#tbCallBack = TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=False)
-
-#create callbacks
-callbacks = [ModelCheckpoint('models/' + filepath + '-{epoch:02d}-{val_acc:.4f}.hdf5'),CSVLogger(filepath + '-history', separator=',', append=False)]
-
 # compile the model (should be done *after* setting layers to non-trainable)
-model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='RMSprop', loss=custom_loss, metrics=['accuracy'])
 #
-datagen = ImageDataGenerator()
 
+for i in range(epochs):
+    for j in range(batches):
+        choices = np.random.choice(x_train_names,batch_size)
+        x_train = np.zeros((0,img_rows,img_cols,3))
+        y_true = np.zeros((0,4))
+        for choice in choices:
+            pic = imio.imread(image_folder + '/' + choice)
+            pic = tform.resize(pic, size)
+            x_train = np.append(x_train, [pic], axis = 0)
 
-#fit the model (should I specify classes?  How do I split the training and test data)
-history = model.fit_generator(datagen.flow_from_directory(directory=train_folder, target_size=size,classes=classes),
-					validation_data=datagen.flow_from_directory(directory=test_folder, target_size=size,classes=classes),
-					validation_steps=len(x_test_names)/batch_size,
-                    epochs=epochs,
-                    steps_per_epoch=len(x_train_names)/batch_size,
-                    callbacks=callbacks,
-                    verbose=1)
+            y_true = np.append(y_true, [bbox[choice]], axis=0)
+
+        y_true = np.reshape(y_true,(batch_size,1,4))
+        y_true = np.tile(y_true,(1,1419,1))
+        print(y_true.shape)
+        model.fit(x_train,y_true,shuffle = False)
+
+    model.save(model_name + str(i) + '.h5')
 
 model.save(filepath)
-#
-# # score = model.evaluate(x_test, y_test, verbose=0)
-#
-# # print('Test loss:', score[0])
-# # print('Test accuracy:', score[1])
-# # score = model.evaluate(x_train, y_train, verbose=0)
-# # print('Training loss:', score[0])
-# # print('Training accuracy:', score[1])
-#
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.xlabel('epochs')
-plt.ylabel('accuracy')
-plt.legend(['train','test'], loc = 'upper left')
-plt.savefig(filepath + '_accuracy.png', bbox_inches='tight')
-#
-#
+
+
+
+
