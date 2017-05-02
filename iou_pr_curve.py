872b6973b9c8dd112be021495e0615b2d954e5df
beerys <sara.beery@gmail.com>
2017-05-02 14:19:41 -0700

    edited

--- iou_pr_curve.py
+++ iou_pr_curve.py
@@ -26,10 +26,10 @@ keras.losses.custom_loss = custom_loss
 
 def get_iou(boxA, boxB):
     # determine the (x, y)-coordinates of the intersection rectangle
-    xA = max(boxA[0], boxB[0])
-    yA = max(boxA[1], boxB[1])
-    xB = min(boxA[2], boxB[2])
-    yB = min(boxA[3], boxB[3])
+    xA = np.max(boxA[0], boxB[0])
+    yA = np.max(boxA[1], boxB[1])
+    xB = np.min(boxA[2], boxB[2])
+    yB = np.min(boxA[3], boxB[3])
 
     # compute the area of intersection rectangle
     interArea = (xB - xA + 1) * (yB - yA + 1)
@@ -94,7 +94,7 @@ for im_name in x_train_names:
     #     conf = conf / np.amax(conf)
     #conf = conf / np.amax(conf)
 
-    y_pred = np.asarray(y_pred[:, :, :-1])
+    y_pred = np.reshape(y_pred[:, :, :-1],(num_boxes,4))
     new_bbox = np.asarray(new_bbox)
     print(new_bbox.shape)
     print(y_pred.shape)
