a8a4a64280679289db9cf562916ef3834fbf81e6
beerys <sara.beery@gmail.com>
2017-05-01 20:26:03 -0700

    edited

--- hw4p1.py
+++ hw4p1.py
@@ -136,10 +136,11 @@ for i in range(epochs):
             print(pic.shape)
             print("x_shape")
             print(x_train.shape)
-            x_train = np.append(x_train, [pic], axis = 0)
-            old_bbox = bbox[choice]
-            new_bbox = [old_bbox[0]*h_ratio, old_bbox[1]*v_ratio, old_bbox[2]*h_ratio, old_bbox[3]*v_ratio]
-            y_true = np.append(y_true, [new_bbox], axis=0)
+            if(pic.ndim == 3):
+                x_train = np.append(x_train, [pic], axis = 0)
+                old_bbox = bbox[choice]
+                new_bbox = [old_bbox[0]*h_ratio, old_bbox[1]*v_ratio, old_bbox[2]*h_ratio, old_bbox[3]*v_ratio]
+                y_true = np.append(y_true, [new_bbox], axis=0)
 
         y_true = np.reshape(y_true,(batch_size,1,4))
         y_true = np.tile(y_true,(1,1419,1))
